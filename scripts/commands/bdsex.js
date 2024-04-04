eb0169e8e7b9ca08caf30e08a683446b62c0d753
Alexis Cadeliña <117723448+Lexizx@users.noreply.github.com>
2024-04-04 12:26:13 +0800

    Update bdsex.js

--- scripts/commands/bdsex.js
+++ scripts/commands/bdsex.js
@@ -1,5 +1,5 @@
 module.exports.config = {
-  name: "sexxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx",
+  name: "sexxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx",
   version: "1.0.0",
   permission: 2,
   credits: "Nayan",
